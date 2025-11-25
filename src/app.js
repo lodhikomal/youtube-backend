ddaa095139c00f58cf573d7c30933d1f4427ed5f
komalrani24 <komal.rani@softradix.in>
2025-11-25 13:28:15 +0530

    utils code discussion

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-console.log(1+2)
+import express from 'express'
+import cors from "cors"
+import cookieParser from 'cookie-parser'
+ const app=express()
+
+ app.use(cors({
+    origin:process.env.CORS_OPTION,
+    crediential:true
+
+ }))
+ app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({
+    extended:true,limit:"16kb"
+}))
+app.use(express.static("public"))
+app.use(cookieParser())
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-
-import express from "express";
-
+import app from "./app.js";
 import { dbConnect } from "./db/index.js";
 import dotenv from "dotenv";
 dotenv.config({
     path:"./env"
 });
 
-const  app=express()
 
 
-
+const port=process.env.PORT||5000;
 
 dbConnect()
+.then(()=>{
+    app.on("error",(error)=>{
+      console.log(error,"error"); 
+          throw error;
+ })
+   app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+})
+.catch((error)=>{
+    console.log(`mongo db connection is failed:${error}`)
+})
+
+
+
+
+
 // (async()=>{
 //     try {
 //    const conectionINstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`)
